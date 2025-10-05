a0961ea250a14baf42fd38a00cb0377a0a536a37
erlonL <erlon.avelino@academico.ufpb.br>
2025-10-05 12:18:05 -0300

    fix2

--- backend/start.sh
+++ backend/start.sh
@@ -5,4 +5,4 @@ cd backend
 pip install -r requirements.txt
 
 # Inicia o servidor FastAPI
-python3 -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
+python3 -m uvicorn main:app --host 0.0.0.0 --port $PORT

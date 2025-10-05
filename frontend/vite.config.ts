faaa381de2b22c2a450aa9415739253e6df6f33d
erlonL <erlon.avelino@academico.ufpb.br>
2025-10-05 09:04:35 -0300

    updated to add github pages files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/space_apps_hackaton/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {

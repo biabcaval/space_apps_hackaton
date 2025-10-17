c75add493361c550d6e7193bdf5b2c9fadca1d55
erlonL <erlon.avelino@academico.ufpb.br>
2025-10-17 17:53:03 -0300

    adjusted logo

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -387,10 +387,8 @@ const fetchAirPollutionForecast = async (lat: number, lon: number) => {
         <header className="bg-gradient-to-r from-blue-yonder via-neon-blue to-electric-blue border-b border-border py-8 px-6 shadow-lg rounded-b-3xl">
           <div className="max-w-7xl mx-auto flex items-center justify-between">
             <div className="flex items-center gap-3">
-              <div className="w-10 h-10 rounded-lg bg-white/20 backdrop-blur-sm flex items-center justify-center">
-                <span className="sm:text-3xl text-xl shadow-sm">
-                  <img src="breez-logo.png" alt="Breez Logo" className="w-full h-full shadow-inner" />
-                </span>
+              <div className="sm:w-16 sm:h-16 w-16 h-10 rounded-lg bg-white/20 backdrop-blur-sm flex items-center justify-center">
+                <img src="breez-logo.png" alt="Breez Logo" className="w-full h-full" />
               </div>
               <div>
                 <h1 className="sm:text-xl text-sm font-bold text-white">Breez</h1>
